691b481fd821cac9393a876366b20d64e1a6c271
zheng_xing <Zheng.Xing@Dell.com>
2021-03-09 22:31:19 +0800

    Add project files.

---
+++ WebClientSignalrTest/wwwroot/js/connect.js
@@ -0,0 +1,18 @@
+﻿
+
+$.connection.hub.url = 'http://127.0.0.1:8889/signalr';
+var hubProxy = $.connection.serverHub;
+
+hubProxy.on("Receviecommand", function () {
+    alert("Receviecommand");
+        });  
+// Start the connection.
+$.connection.hub.start()
+    .done(function () {
+        hubProxy.invoke('send');
+
+        
+    })
+    .fail(function () {
+        alert("failed in connecting to the signalr server");
+    })
